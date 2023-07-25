68a98fd256c5a24513ffce77361d6d335c1e1db7
Marceau Rodrigues <marceau0707@gmail.com>
2023-07-25 22:31:01 +0200

    https://openclassrooms.com/fr/courses/7008001-debutez-avec-react/7135822-reutilisez-vos-composants-avec-les-props

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
+function Banner() {
+    return (
+        <div className="lmj-banner">
+            <img src={logo} alt="La maison jungle" className="lmj-logo" />
+
+            <h1 className="lmj-title">La maison jungle</h1>
+        </div>
+    )
+}
+
+export default Banner
